d3202309b0d50ca912c3bae8d482bb87f647dcf6
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-09-24 11:58:16 +0100

    even more initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import NavBarTop from "./components/NavBarTop";
+import Home from "./views/Home";
+import SixteenDays from "./views/SixteenDays";
+import WatchList from "./views/WatchList";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Router>
         <NavBarTop />
         <Container className="mt-5"></Container>
+        <Route path="/" exact component={Home} />
+        <Route path="/watchList" exact component={WatchList} />
+        <Route path="/sixteenDays:cityId" exact component={SixteenDays} />
       </Router>
     </div>
   );
